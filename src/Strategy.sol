bdc4fe05b1a997b660a88bf0f51661bfb0dbc561
charlesndalton <charlesndalton@gmail.com>
2022-04-22 08:49:54 -0700

    feat: add Bancor interfaces

--- src/Strategy.sol
+++ src/Strategy.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0
-// Feel free to change the license, but this is what we use
 
 pragma solidity ^0.8.12;
 pragma experimental ABIEncoderV2;
---
+++ src/interfaces/Bancor/IPoolToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.13;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
+
+/**
+ * @dev Pool Token interface
+ */
+interface IPoolToken is IERC20, IERC20Permit {
+    /**
+     * @dev returns the address of the reserve token
+     */
+    function reserveToken() external view returns (Token);
+
+    /**
+     * @dev increases the token supply and sends the new tokens to the given account
+     *
+     * requirements:
+     *
+     * - the caller must be the owner of the contract
+     */
+    function mint(address recipient, uint256 amount) external;
+}
